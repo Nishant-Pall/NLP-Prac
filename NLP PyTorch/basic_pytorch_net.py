e1f476b18b7aa7f0e125f11170f0d13654040a6d
nishant <palln6935@gmail.com>
2020-08-31 22:50:05 +0530

    build and train the basic net

--- NLP PyTorch/basic_pytorch_net.py
+++ NLP PyTorch/basic_pytorch_net.py
@@ -22,6 +22,79 @@ class NN(nn.Module):
         return x
 
 
-model = NN(784, 10)
-x = torch.rand(64, 784)
-print(model(x))
+# Hyperparameters
+input_size = 784
+num_classes = 10
+learning_rate = 0.001
+batch_size = 64
+num_epochs = 1
+
+# Load Data
+train_dataset = datasets.MNIST(
+    root='dataset/', train=True, transform=transforms.ToTensor(), download=True)
+train_loader = DataLoader(dataset=train_dataset,
+                          batch_size=batch_size, shuffle=True)
+test_dataset = datasets.MNIST(
+    root='dataset/', train=False, transform=transforms.ToTensor(), download=True)
+test_loader = DataLoader(dataset=test_dataset,
+                         batch_size=batch_size, shuffle=True)
+
+# Initialize network
+model = NN(input_size=input_size, num_classes=num_classes)
+
+# Loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+# Train Network
+
+for epoch in range(num_epochs):
+    for batch_idx, (data, targets) in enumerate(train_loader):
+        # Get data to device
+        data = data.to(device='cpu')
+        targets = targets.to(device='cpu')
+        # Reshape to correct shape
+        data = data.reshape(data.shape[0], -1)
+
+        # forward
+        scores = model(data)
+        loss = criterion(scores, targets)
+
+        # backward
+        optimizer.zero_grad()
+        loss.backward()
+
+        # gradient descent
+        optimizer.step()
+
+# Check accuracy on training &test to see how good our model is
+
+
+def check_accuracy(loader, model):
+    if loader.dataset.train:
+        print("Checking acc on training data")
+    else:
+        print("Checking acc on test data")
+    num_correct = 0
+    num_samples = 0
+    model.eval()
+
+    with torch.no_grad():
+        for x, y in loader:
+            x = x.to(device='cpu')
+            y = y.to(device='cpu')
+            x = x.reshape(x.shape[0], -1)
+
+            scores = model(x)
+            _, predictions = scores.max(1)
+            num_correct += (predictions == y).sum()
+            num_samples += predictions.size(0)
+
+        print(
+            f'Got {num_correct} / {num_samples} with accuacy {float(num_correct)/float(num_samples)*100:.2f}')
+
+    model.train()
+
+
+check_accuracy(train_loader, model)
+check_accuracy(test_loader, model)
